037acb00a2e0ebb35d910730574bd6af2bcb3af7
Heba-Webdev23 <jtvonline@yahoo.com>
2023-09-19 23:53:01 +0300

    Added useAuthContext global state

--- app/page.js
+++ app/page.js
@@ -1,7 +1,14 @@
-export default function Home() {
-  return (
-    <main className="">
-      <h1>Main entry</h1>
-    </main>
-  );
+"use client";
+import { useEffect } from "react";
+import { useAuthContext } from "@/context/AuthContext";
+import { useRouter } from "next/navigation";
+export default function Page() {
+  const { user } = useAuthContext();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user == null) router.push("/login");
+  }, [user, router]);
+
+  return <main>{user && <h1>Welcome home</h1>}</main>;
 }
