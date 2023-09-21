c4857459185f6f31c7c162171c8a7d1bbef35501
Heba-Webdev23 <jtvonline@yahoo.com>
2023-09-21 19:53:26 +0300

    Created the file the contains the images

---
+++ data.js
@@ -0,0 +1,38 @@
+const data = [
+  {
+    id: 0,
+    img_path: "/images/tech/internetofthings.jpg",
+    alt: "Internet of Things",
+    tags: ["Tech", "IoT"],
+  },
+  {
+    id: 1,
+    img_path: "/images/tech/software.jpg",
+    alt: "Mobile App Development",
+    tags: ["Tech", "Apps Development"],
+  },
+  {
+    id: 2,
+    img_path: "/images/tech/robotics.jpg",
+    alt: "Robotics",
+    tags: ["Tech", "Robotics"],
+  },
+  {
+    id: 3,
+    img_path: "/images/tech/virtualreialty.jpg",
+    alt: "Virtual Reality",
+    tags: ["Tech", "Virtual Reality"],
+  },
+  {
+    id: 4,
+    img_path: "/images/tech/network.jpg",
+    alt: "Network",
+    tags: ["Tech", "Network"],
+  },
+  {
+    id: 5,
+    img_path: "/images/tech/videogames.jpg",
+    alt: "Video Games Development",
+    tags: ["Tech", "Games Development"],
+  },
+];
